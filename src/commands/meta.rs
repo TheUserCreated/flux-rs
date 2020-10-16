9431fc5b1dc9632ee132ff9f7494759224f0aa0a
TheUserCreated <boswinle001@gmail.com>
2020-10-15 21:09:12 -0500

    Initial commit. This sets up the command framework, along with a basic ping and basic die command.

---
+++ src/commands/meta.rs
@@ -0,0 +1,61 @@
+use serenity::client::bridge::gateway::ShardId;
+use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+use crate::ShardManagerContainer;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let data = ctx.data.read().await;
+    let shard_manager = match data.get::<ShardManagerContainer>() {
+        Some(v) => v,
+        None => {
+            msg.reply(ctx, "There was a problem getting the shard manager")
+                .await?;
+            return Ok(());
+        }
+    };
+    let manager = shard_manager.lock().await;
+    let runners = manager.runners.lock().await;
+
+    //shards are backed by runners that are responsible for the shards events
+    //so to get the latency we gotta get some info from the current runner
+    let runner = match runners.get(&ShardId(ctx.shard_id)) {
+        Some(runner) => runner,
+        None => {
+            msg.reply(ctx, "No shard found. How did that even happen?")
+                .await?;
+            return Ok(());
+        }
+    };
+    let latency = match runner.latency {
+        Some(latency) => latency,
+        None => {
+            msg.reply(ctx, "This shard hasn't been active long enough to measure its own latency. Please wait.").await?;
+            return Ok(());
+        }
+    };
+    msg.reply(ctx, &format!("The shard latency is {:?}", latency))
+        .await?;
+
+    Ok(())
+}
+
+#[command]
+#[owners_only]
+async fn die(ctx: &Context, msg: &Message) -> CommandResult {
+    let data = ctx.data.read().await;
+
+    if let Some(manager) = data.get::<ShardManagerContainer>() {
+        msg.reply(ctx, "Shutting down!").await?;
+        manager.lock().await.shutdown_all().await;
+    } else {
+        msg.reply(ctx, "There was a problem getting the shard manager")
+            .await?;
+
+        return Ok(());
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+mod commands;
+
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::{event::ResumedEvent, gateway::Ready},
+    prelude::*,
+};
+use std::{collections::HashSet, env, sync::Arc};
+
+use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+use commands::meta::*;
+
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(ping, die)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("Failed to load env file");
+    //this next boi logs the env variables.
+    //and yes, this portion of the code is essentially the serenity example part
+    //i don't see any reason to do it differently seeing as this is exactly what i need
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to start the logger");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = Http::new_with_token(&token);
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(owners).prefix("."))
+        .group(&GENERAL_GROUP);
+    let mut client = Client::builder(&token)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
